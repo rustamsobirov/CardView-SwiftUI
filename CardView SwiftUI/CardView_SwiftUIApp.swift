9518c93c52a2edded6a64db9ab6eb8d0bf013955
admin <carwolt123@gmail.com>
2023-03-03 22:51:20 +0500

    Initial Commit

---
+++ CardView SwiftUI/CardView_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardView_SwiftUIApp.swift
+//  CardView SwiftUI
+//
+//  Created by iMac G1 on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CardView_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
